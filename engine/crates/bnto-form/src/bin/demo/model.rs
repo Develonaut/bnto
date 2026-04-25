a532fec6e6e3ec04d172836cef6bed80244ea760
Ryan McHenry <ryanmmchenry@gmail.com>
2026-04-24 21:34:41 -0500

    Fix clippy unnecessary_sort_by lint on Rust 1.95

--- engine/crates/bnto-form/src/bin/demo/model.rs
+++ engine/crates/bnto-form/src/bin/demo/model.rs
@@ -169,8 +169,8 @@ fn read_directory(
         }
     }
 
-    dirs.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
-    files.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
+    dirs.sort_by_key(|a| a.name.to_lowercase());
+    files.sort_by_key(|a| a.name.to_lowercase());
     dirs.append(&mut files);
     dirs
 }
